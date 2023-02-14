874cd871f3694a463d0c0e3f067ce3fc76caa91b
giboulette <108846731+giboulette@users.noreply.github.com>
2023-02-14 19:32:12 +0100

    Dead weight and surface loads added

--- ThreeHingedArch.py
+++ ThreeHingedArch.py
@@ -9,6 +9,7 @@ colors = dict(
     arch='k',
     load='r',
     force='g',
+    shear='m',
     moment='b'
 )
 alpha = 0.3
@@ -31,12 +32,14 @@ def integrate(
 
 def arch_shape(x: numpy_array, L: float, H: float, k: float) -> numpy_array:
     y = x*(k*L-x)*4/(L*(2*k*L-L))*H
-    yr = y[x <= L/2]
-    return np.concatenate((yr, yr[::-1]))
+    yl = y[x <= L/2]
+    return np.concatenate((yl, yl[::-1]))
 
 
 def arch_deriv(x: numpy_array, L: float, H: float, k: float) -> numpy_array:
-    return (k*L-2*x)*4/(L*(2*k*L-L))*H
+    d = (k*L-2*x)*4/(L*(2*k*L-L))*H
+    dl = d[x <= L/2]
+    return np.concatenate((dl, -dl[::-1]))
 
 
 class ThreeHingedArch:
@@ -63,162 +66,177 @@ class ThreeHingedArch:
             H: float = 1,
             k: float = 1,
             num: int = 1000,
-            vertical_load: Callable = lambda x: np.exp(x),
-            right_load: Callable = lambda x: (x-x.min())/(x.max()-x.min()),
-            left_load: Callable = lambda x: np.ones_like(x),
+            vertical_load: Callable = lambda x: np.zeros_like(x),
+            right_load: Callable = lambda y: np.zeros_like(y),
+            left_load: Callable = lambda y: np.zeros_like(y),
+            surface_load: Callable = lambda x: np.zeros_like(x),
+            density: float = 0,  # N/m
+            colors: dict[str: str] = colors,
+            alpha: float = alpha
     ) -> None:
 
-        self.L = L
-        self.H = H
-        self.k0 = k
+        # Geometry is entirely defined from these 3 parameters
+        self.L = L  # Lenght
+        self.H = H  # Height
+        self.k0 = k  # top angle coeficient
 
+        self.colors = colors  # Diagram & plot colors
+        self.alpha = alpha  # Diagram fills transparency
+
+        # numerical shape
         self.x = np.linspace(0, L, num=num)
         self.y = arch_shape(self.x, L, H, k)
         self.derivative = arch_deriv(self.x, L, H, k)
-        self.nv = np.array(
-            (-self.derivative, np.ones_like(self.derivative))
+
+        # Left side
+        left = self.x <= self.L/2
+        self.xl = self.x[left]
+        self.yl = self.y[left]
+
+        # Rigth side
+        right = self.x > self.L/2
+        self.xr = self.x[right]
+        self.yr = self.y[right]
+
+        # direction vector
+        self.dv = np.array(
+            (np.ones_like(self.derivative), self.derivative)
         )/np.sqrt(1+self.derivative**2)
+        self.dl = np.linalg.norm((np.ones(num), self.derivative), axis=0)
+        # normal vector
+        self.nv = np.array((-self.dv[1, :], self.dv[0, :]))
+        # Attaching the essentials to the object
         self.geometry = (self.x, self.y, self.L, self.H)
 
-        self.v = vertical_load(self.x)
-        self.hl = left_load(self.y[self.x <= L/2])
-        self.hr = right_load(self.y[self.x > L/2])
-        self.hr = self.hr[::-1]
-        self.h = np.concatenate((self.hl, self.hr))
+        dead_weight = self.dl*density
+        sf = self.dl*surface_load(self.x)
+        sfx, sfy = sf*np.abs(self.nv) * self.dl
 
-        self.calc_constants()
-        self.calc_reactions()
-        self.calc_stresses()
+        self.v = vertical_load(self.x) + dead_weight + sfy
+        self.vl = self.v[left]
+        self.vr = self.v[right]
+        self.hl = left_load(self.yl) + sfx[left]
+        self.hr = right_load(self.yr)[::-1] + sfx[right]
 
-    def calc_constants(self):
+        self.calculate()
 
-        x, y, L, H = self.geometry
-        v = self.v
+    def calculate(self):
+        values, arrays = self.calc_loads()
+        reactions = self.calc_reactions(values)
+        return self.calc_stresses(arrays, reactions)
 
-        # Left arch
-        left = x <= L/2
-        self.xl = x[left]
-        self.yl = y[left]
-        self.vl = v[left]
+    def calc_loads(self):
+
+        # Vertical left load
         Vls = integrate(self.xl, self.vl)
-        Vls_cls = integrate(self.xl, self.vl*self.xl)
+        # Vertical left load's moment
+        Mvls = integrate(self.xl, self.vl*self.xl)
         Vl = Vls[-1]
-        Vl_cl = Vls_cls[-1]
+        Mvl = Mvls[-1]
+
+        # Horizontal left load
         Hls = integrate(self.yl, self.hl)
-        Hls_cls = integrate(self.yl, self.hl*self.yl)
+        # Horizontal left load's moment
+        Mhls = integrate(self.yl, self.hl*self.yl)
         Hl = Hls[-1]
-        Hl_cl = Hls_cls[-1]
+        Mhl = Mhls[-1]
 
-        # Rigth arch
-        right = x > L/2
-        self.xr = x[right]
-        self.yr = y[right]
-        self.vr = v[right]
-        Vrs = integrate(self.xr - L/2, self.vr)
-        Vrs_crs = integrate(self.xr - L/2, self.vr*(self.xr-L/2))
+        # Vertical right load
+        Vrs = integrate(self.xr, self.vr)
+        # Vertical right load's moment
+        Mvrs = integrate(self.xr, self.vr*(self.xr-self.L/2))
         Vr = Vrs[-1]
-        Vr_cr = Vrs_crs[-1]
-        Hrs = integrate(self.H - self.yr, self.hr)
-        Hrs_crs = integrate(self.H - self.yr, self.hr*(self.H - self.yr))
+        Mvr = Mvrs[-1]
+
+        # Horizontal right load
+        Hrs = -integrate(self.yr, self.hr)
+        # Horizontal right load's moment
+        Mhrs = -integrate(self.yr, self.hr*(self.H - self.yr))
         Hr = Hrs[-1]
-        Hr_cr = Hrs_crs[-1]
+        Mhr = Mhrs[-1]
 
-        V = integrate(x, v, cumul=False)
-        H = Hl - Hr
-
-        self.constants = (
-            (Vr, Vrs, Vr_cr, Vrs_crs),
-            (Vl, Vls, Vl_cl, Vls_cls),
-            (Hr, Hrs, Hr_cr, Hrs_crs),
-            (Hl, Hls, Hl_cl, Hls_cls),
-            (V, H)
+        return (
+            # Load sums for the reaction's calculation
+            (Vr, Mvr, Vl, Mvl,
+             Hr, Mhr, Hl, Mhl),
+            # Load arrays for moment and normal force distribution
+            (Vrs, Mvrs, Vls, Mvls,
+             Hrs, Mhrs, Hls, Mhls)
         )
-        return self.calc_constants
 
-    def calc_reactions(self):
+    def calc_reactions(self, load_values):
 
-        (
-            (Vr, Vrs, Vr_cr, Vrs_crs),
-            (Vl, Vls, Vl_cl, Vls_cls),
-            (Hr, Hrs, Hr_cr, Hrs_crs),
-            (Hl, Hls, Hl_cl, Hls_cls),
-            (V, H)
-        ) = self.constants
+        (Vr, Mvr, Vl, Mvl,
+         Hr, Mhr, Hl, Mhl) = load_values
 
         A = np.array([
             # Ax  Ay       Bx         By         Cx         Cy
             [1,    0,      -1,         0,        0,         0],
-            [0,    0,       1,         0, -      1,         0],
+            [0,    0,       1,         0,       -1,         0],
             [0,    1,       0,         1,        0,         0],
             [0,    0,       0,        -1,        0,         1],
             [0,    0,  self.H,  self.L/2,        0,         0],
             [0,    0,       0,         0,  -self.H,  self.L/2],
         ])
         b = np.array([
-            -Hl,
-            +Hr,
-            Vl,
-            Vr,
-            Vl_cl+Hl_cl,
-            Vr_cr+Hr_cr
+            -Hl,  # Ax - Bx =-Hl
+            +Hr,  # Bx - Cx = Hr
+            Vl,  # Ay + By = Vl
+            Vr,  # -By + Cy = Vr
+            Mvl+Mhl,  # Bx*H + By*L/2 = Mvl + Mhl
+            Mvr+Mhr  # -Cx*H + Cy*L/2 = Mvr + Mhr
         ])
-        self.reactions = np.linalg.solve(A, b)
 
-        print(f"A       = {A}")
-        print(f"b       = {b}")
-        print(f"R       = {self.reactions}")
-        print(f"Forces  = {[V, Vl, Vr, H, Hl, Hr]}")
-        print(f"Moments = {[Vl_cl, Vr_cr, Hl_cl, Hr_cr]}")
+        return np.linalg.solve(A, b)
 
-        return self.reactions
+    def calc_stresses(self, load_arrays, reactions):
 
-    def calc_stresses(self):
+        (Vrs, Mvrs, Vls, Mvls,
+         Hrs, Mhrs, Hls, Mhls) = load_arrays
 
-        (
-            (Vr, Vrs, Vr_cr, Vrs_crs),
-            (Vl, Vls, Vl_cl, Vls_cls),
-            (Hr, Hrs, Hr_cr, Hrs_crs),
-            (Hl, Hls, Hl_cl, Hls_cls),
-            (V, H)
-        ) = self.constants
-        Ax, Ay, Bx, By, Cx, Cy = self.reactions
+        Ax, Ay, Bx, By, Cx, Cy = reactions
 
-        Ml = (Ax+Hls)*self.yl - (Ay-Vls)*self.xl - Hls_cls - Vls_cls
-        # Mr = (
-        #     Vrs_crs + Hls_cls
-        #     - By*(self.xr-self.L/2) - Bx*(self.H-self.yr)
-        # )
+        Ml = (
+            (Ax+Hls)*self.yl - (Ay-Vls)*self.xl
+            - Mhls - Mvls
+        )
         Mr = (
-            - Vrs_crs - Hrs_crs
+            - Mvrs - Mhrs
             - (Bx-Hrs)*(self.H-self.yr) + (By+Vrs)*(self.xr-self.L/2)
         )
 
         self.M = np.concatenate((Ml, Mr))
 
-        Nl = np.linalg.norm(np.array((
-            Ax + Hls, Ay - Vls
-        )), axis=0)
-        Nr = np.linalg.norm(np.array((
-            Bx - Hrs, By + Vrs
-        )), axis=0)
-        self.N = np.concatenate((Nl, Nr))
+        Fx = np.concatenate((Ax+Hls, Bx-Hrs))
+        Fy = np.concatenate((Vls-Ay, By+Vrs))
+        F = np.array([Fx, Fy])
+        self.N = (F*self.dv).sum(axis=0)
+        self.V = (F*self.nv).sum(axis=0)
+
+        return self.M, self.N, self.V
 
     def diagram(self,
                 loads_scale=1.5,
                 moments_scale=1.5,
-                force_scale=4):
+                force_scale=4,
+                show=True):
 
+        # In order to scale the moment diagram to the maximum values
+        moments_scale *= np.abs(self.M).max()
+        force_scale *= np.abs(self.N).max()
         loads_scale *= max(
             np.abs(load).max() for load in (self.v, self.hr, self.hl)
         )
-        moments_scale *= np.abs(self.M).max()
-        force_scale *= np.abs(self.N).max()
 
+        # Check if it is null to avoid divisions by 0
         loads_scale = 1 if loads_scale == 0 else loads_scale
         moments_scale = 1 if moments_scale == 0 else moments_scale
         force_scale = 1 if force_scale == 0 else force_scale
 
+        plt.axis('off')
+        # Arch
         plt.plot(self.x, self.y, '-k', lw=3)
+        # Hinges
         plt.scatter((0, self.L/2, self.L), (0, self.H, 0), s=50,
                     facecolor='w', linewidths=2, edgecolors='k', zorder=3)
 
@@ -233,12 +251,21 @@ class ThreeHingedArch:
         )
 
         Nx, Ny = self.N*self.nv/force_scale
+        Vx, Vy = self.V*self.nv/force_scale
         normal, = plt.fill(
             np.concatenate((self.x+Nx, self.x[::-1])),
             np.concatenate((self.y+Ny, self.y[::-1])),
             edgecolor='none',
             color=colors['force'],
             label="Normal force",
+            alpha=alpha
+        )
+        shear, = plt.fill(
+            np.concatenate((self.x+Vx, self.x[::-1])),
+            np.concatenate((self.y+Vy, self.y[::-1])),
+            edgecolor='none',
+            color=colors['shear'],
+            label="Shear force",
             alpha=alpha
         )
 
@@ -272,7 +299,7 @@ class ThreeHingedArch:
         if not (self.hr == 0).all():
             load, = plt.fill(
                 np.concatenate((
-                    1.1*self.L + self.hr/loads_scale,
+                    1.1*self.L + self.hr[::-1]/loads_scale,
                     1.1*self.L + np.zeros_like(self.hl)
                 )),
                 np.concatenate((
@@ -283,37 +310,55 @@ class ThreeHingedArch:
                 label="Loads",
                 alpha=alpha
             )
-        lines = [moment, normal] + [load]
-        labs = [line.get_label() for line in lines]
-        plt.legend(lines, labs, loc='lower center')
-        plt.show()
 
-    def plot(self):
+        # Grouping labels into the axis' legend
+        if load is None:
+            lines = (moment, normal, shear)
+            print("Warning: no loads found")
+        else:
+            lines = (moment, normal, shear, load)
+        labs = [line.get_label() for line in lines]
+        plt.legend(lines, labs)
+
+        if show:
+            plt.show()
+
+    def plot(self, show_loads=True, show=True):
 
         fig, ax1 = plt.subplots(1, sharex=True, figsize=(8, 6))
         ax2 = ax1.twinx()
         ax1.axline((0, 0), slope=0, color='k', linestyle='--', lw=1)
         ax2.axline((0, 0), slope=0, color='k', linestyle='--', lw=1)
-        lns1, = ax1.plot(self.x, self.M, '--', label="Moments",
-                         color=colors['moment'], lw=2)
-        lns2, = ax2.plot(self.x, self.N, '--', label="Normal force",
-                         color=colors['force'], lw=2)
-        lns3, = ax2.plot(self.x, self.v, '-.',
-                         color=colors['load'], label='Vertical load', lw=1)
-        lns4, = ax2.plot(self.x, self.h, ':',
-                         color=colors['load'], label='Horizontal load', lw=1)
+        moment, = ax1.plot(self.x, self.M, '--', label="Moments",
+                           color=colors['moment'], lw=2)
+        normal, = ax2.plot(self.x, self.N, '-.', label="Normal force",
+                           color=colors['force'], lw=1.5)
+        shear, = ax2.plot(self.x, self.V, '-.', label="Shear force",
+                          color=colors['shear'], lw=1.5)
 
-        lns = (lns1, lns2, lns3, lns4)
+        if show_loads:
+            lns3, = ax2.plot(self.x, self.v, '-.',
+                             color=colors['load'],
+                             label='Vertical load', lw=1)
+            lns4, = ax2.plot(self.x, np.concatenate((self.hl, self.hr)), ':',
+                             color=colors['load'],
+                             label='Horizontal load', lw=1)
+            lns = (moment, normal, shear, lns3, lns4)
+        else:
+            lns = (moment, normal, shear)
+
+        # Merging labels into one legend
         labs = [ln.get_label() for ln in lns]
         ax2.legend(lns, labs, loc=(0.01, 1.0), ncols=4)
         ax1.set_xlabel("Position [m]")
         ax1.set_ylabel("Moment [N$\\cdot$m]", color=colors["moment"])
-        ax2.set_ylabel("Force [N]", color=colors["force"])
-        ax2.spines['left'].set_color(colors["moment"])
-        ax2.spines["right"].set_color(colors["force"])
+        ax2.set_ylabel("Force [N]")
+
+        # Detail colors
         ax1.tick_params(colors=colors["moment"], axis="y")
-        ax2.tick_params(colors=colors["force"], axis="y")
-        plt.show()
+
+        if show:
+            plt.show()
 
 
 if __name__ == "__main__":
@@ -321,19 +366,24 @@ if __name__ == "__main__":
     def horizontal_load_right(x: numpy_array):
         return np.zeros_like(x)
 
-    def horizontal_load_left(x: numpy_array):
+    def vertical_load(x: numpy_array):
         return (x-x.min())/(x.max()-x.min())
 
-    def vertical_load(x: numpy_array):
-        return np.exp(horizontal_load_left(x))
+    def horizontal_load_left(x: numpy_array):
+        return np.exp(vertical_load(x))*0
+
+    def surface_load(x: numpy_array):
+        return np.ones_like(x)
 
     arch = ThreeHingedArch(
         L=4,  # length
         H=3,  # height
-        vertical_load=vertical_load,
-        right_load=horizontal_load_right,
-        left_load=horizontal_load_left
+        k=1.0,
+        surface_load=surface_load,
+        # vertical_load=vertical_load,
+        # right_load=horizontal_load_right,
+        # left_load=horizontal_load_left
     )
     arch.diagram()
     plt.style.use('bmh')
-    arch.plot()
+    arch.plot(show_loads=False)
